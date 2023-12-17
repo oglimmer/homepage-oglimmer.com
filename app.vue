31b0747fe6b3e36890093223d6fc5318c6d54ae7
Oli Zimpasser <oglimmer@gmail.com>
2023-12-17 23:58:49 +0100

    Use icons for light/dark mode

--- app.vue
+++ app.vue
@@ -315,6 +315,11 @@ const transformArray = (arr: any) => {
   return result;
 }
 
+const switchColorMode = () => {
+  colorMode.value = colorMode.value === 'light' ? 'dark' : 'light';
+  return false;
+}
+
 </script>
 
 <template>
@@ -323,17 +328,15 @@ const transformArray = (arr: any) => {
     <nav class="navbar navbar-expand-lg py-3 navbar-dark">
       <div class="container-fluid">
         <a class="navbar-brand font-weight-bold text-uppercase" href="#"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div>
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <select class="form-select form-select-sm" v-model="$colorMode.preference">
-                <option value="system">System color</option>
-                <option value="light">Light</option>
-                <option value="dark">Dark</option>
-              </select>
+              <a href="javascript:void(0);" :onClick="switchColorMode">
+                <span style="font-size: 1.4em;">
+                  <span v-if="colorMode.value === 'light'">☼</span>
+                  <span v-if="colorMode.value === 'dark'">☾</span>
+                </span>
+              </a>
             </li>
           </ul>
         </div>
@@ -403,4 +406,14 @@ div p {
   justify-content: center;
 }
 
+a {
+  text-decoration: none;
+  color: inherit;
+  outline: none;
+}
+
+a:hover {
+  text-decoration: none;
+  color: inherit;
+}
 </style>
